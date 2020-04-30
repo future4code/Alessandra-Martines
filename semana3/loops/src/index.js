aa765ef956e7f5d1b7c992be8e8ccbfd4f233b73
alessandra-leka <leka.produtora@gmail.com>
2020-04-29 21:22:49 -0300

    Finalizei os exercícios e fiz o desafio 1

--- semana3/loops/src/index.js
+++ semana3/loops/src/index.js
@@ -1,7 +1,6 @@
-const array = [10, 163, 200, 30, 50, 530]
-for(let i = 0;i <= 10; i++){
-    if (i % 2 === 0){ //quando o resto da divisão por 2 for 0, ele é par//    
-        console.log('${í} é par')
-    } else{
-        console.log('${í} é ímpar')
-    }
+let array = [1, 2, 3, 4, 5, 6]
+let arrayNovo = []
+for (let i = 0; i < array.length; i++) {
+    arrayNovo.push(`O elemento index ${i} é ${array[i]}`)
+}
+console.log(arrayNovo)
