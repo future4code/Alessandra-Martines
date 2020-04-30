3dc04e602f410a56537675e3ab015f8f8b44d555
alessandra-leka <leka.produtora@gmail.com>
2020-04-30 16:58:10 -0300

    Inicio do exercíciodd

---
+++ semana3/blackjack/nossoJogo.js
@@ -0,0 +1,35 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+let carta1 = comprar ()
+let carta2 = comprar ()
+let carta3 = comprar ()
+let carta4 = comprar ()
+let carta5 = comprar ()
+let valor1 = carta1.valor + carta2.valor
+let valor2 = carta3.valor + carta4.valor
+let valor3 = valor1 + carta6.valor
+let valor4 = valor3 + carta7.valor
+
+ let boasVindas = ("Bem vindo ao jogo de Blackjack!")
+ console.log(boasVindas)
+
+ if(confirm("Quer iniciar uma nova rodada?")) {
+   if(confirm("")
+} else {
+   
+}
+   console.log("O jogo acabou")
+}
+
+
+
