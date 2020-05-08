6d591492e898cda53883f99b6154af684bd368e7
alessandra-leka <leka.produtora@gmail.com>
2020-05-08 02:21:57 -0300

    Finalizei o filtro e dei uma leve estilizada

--- semana4/funcoes-de-array/fucDeArray.js
+++ semana4/funcoes-de-array/fucDeArray.js
@@ -1,9 +1,11 @@
 //Descrevendo o objeto
 let controlador = {
     valorReal: '',
-    tipo: ['casa', 'viagem', 'rolê', 'outros'],
+    tipo: '' ,
     descricao: '',
 } 
+
+let array = []
 
 //Capturando valores do HTML
 function valoresDetalhado(){
@@ -11,8 +13,7 @@ function valoresDetalhado(){
     let valorRealImpresso = document.getElementById('valorReal').value
     let tipoImpresso = document.getElementById('tipo').value
     let descricaoImpresso = document.getElementById('descricao').value
-    let array = []
-
+    
     controlador.valorReal = valorRealImpresso
     controlador.tipo = tipoImpresso
     controlador.descricao = descricaoImpresso
@@ -21,5 +22,42 @@ function valoresDetalhado(){
     console.log(array)
 
     document.getElementById('impressaovalores').innerHTML += `<h1>${controlador.valorReal}</h1><br><h2>${controlador.tipo}</h2><br><b>${controlador.descricao}</b>`
+
+}
+
+
+const captadorDeFiltro = () =>{
+    event.preventDefault()
+    let tipoFiltro = document.getElementById("tipoFiltro").value
+    let valorMinimo = +document.getElementById("minimo").value
+    let valorMaximo = +document.getElementById("maximo").value
+    
+    let caixaDeFiltro = []
+
+    if(valorMinimo == '' || valorMaximo == ''){
+        alert('Necessário preenchimento')
+    } else {
+        let filtro = array.filter((index) =>{
+            if(valorMinimo <= index.valorReal && valorMaximo >= index.valorReal && tipoFiltro === index.tipo){
+            caixaDeFiltro.push(index)
+            console.log(caixaDeFiltro)
+            } else {
+                alert('Valores não declarados!')
+            }
+        })
+        console.log(filtro)
+    }
+    for (i = 0; i < caixaDeFiltro.length; i++){
+        document.getElementById("impressaofiltros").innerHTML += `<div>
+        <h2>${caixaDeFiltro[i].descricao}</h2>
+        <p>${caixaDeFiltro[i].tipo}</p>
+        <p>${caixaDeFiltro[i].valorReal}</p>
+        </div>`
+    }    
+
+}
+
+const limparFiltro = () =>{
+    let limpaInput = document.getElementsByTagName ("input").value = ""
 }
 
