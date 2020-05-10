176788979c9948eecdf0171b61a46244f4eb6d52
alessandra-leka <leka.produtora@gmail.com>
2020-05-10 11:03:09 -0300

    Exercício 1 concluído

---
+++ semana4/lista-de-exercicio/script.js
@@ -0,0 +1,17 @@
+//INTERPRETAÇÃO DE CÓDIGOS
+//RESPOSTA 1 - A função é para converter dólar para real. Foi criada a variável de cotação, que puxa o valor do dólar do 
+// dias no prompt (serve para perguntar e digitar a resposta) em número. Ele retorna (return) uma string mais uma operação
+// de valor do dolar * o valor do prompt declarado. Fora do escopo, ele declara a constante do dinheiro a ser convertido e
+//pede para imprimir o valor (ele vai imprimir no console o valor que foi digitado * 100, por exemplo: digito 5, ele imprime R$500)
+
+//RESPOSTA 2 - Ele usa o switch para comparar as variáveis e declara o primeiro sendo os 150 * 1.1 = 165 e da alert pra 
+// a segunda, já que ela não está declarada nas cases.
+
+//RESPOSTA 3 - foi declarada uma array com 14 números, em seguida duas arrays vazias. Em seguida ele usa um loop para
+//percorrer a array declarada com a ação de se o número dividido por 2 ter o resto 0, ele armazena no array1 e imprime  
+//quantas tem nessa array (6). Agora, se o resto da divisão por 2 não for 0, ele armazena no array2 e imprime quantas tem
+//nesse array (8). Mas antes dessas impressões ele imprime a string + o total da array declarada (Quantidade total de número 14)
+
+//RESPOSTA 4 - foi declarada uma constante de números uma variável indinita e uma zero. E ele pede no loop para achar o
+// menor e o maior número e colocar nas variáveis declaradas. (numero1 = -10 numero2 = 1950)
+
