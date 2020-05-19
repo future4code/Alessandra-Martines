835c009ba581eadbe17b70a1f7ab06508f3ba96f
alessandra-leka <leka.produtora@gmail.com>
2020-05-19 14:28:01 -0300

    ...

---
+++ semana6/formulario/build/precache-manifest.bf0943d63727d791268ab45279763cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5641aa46d20c1ffd04207aa0855873e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2713d104e9ea66f5f176",
+    "url": "/static/css/main.bc68d007.chunk.css"
+  },
+  {
+    "revision": "f005f9ce8137e72c0d9d",
+    "url": "/static/js/2.9d4ffca2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d4ffca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2713d104e9ea66f5f176",
+    "url": "/static/js/main.ebace5fe.chunk.js"
+  },
+  {
+    "revision": "6b29fae4af0e617ef28d",
+    "url": "/static/js/runtime-main.26ef853f.js"
+  }
+]);
