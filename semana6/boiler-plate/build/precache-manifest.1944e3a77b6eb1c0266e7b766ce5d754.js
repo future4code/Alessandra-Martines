d3ef101ef23817ed8862f4d3f4899b8c955af8c7
alessandra-leka <leka.produtora@gmail.com>
2020-05-25 22:30:30 -0300

    Build do exercício

---
+++ semana6/boiler-plate/build/precache-manifest.1944e3a77b6eb1c0266e7b766ce5d754.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ee4419dbf76476ce19da3e58145eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e84e05006fc6d817ce",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "078d8750ac3017505296",
+    "url": "/static/js/2.29e9a946.chunk.js"
+  },
+  {
+    "revision": "16e84e05006fc6d817ce",
+    "url": "/static/js/main.41bae6e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
