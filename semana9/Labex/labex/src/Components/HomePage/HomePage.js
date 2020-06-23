c33d9d2effb445db0d3f116db4882a532344e453
alessandra-leka <leka.produtora@gmail.com>
2020-06-23 19:47:46 -0300

    Apliquei o Router, mas ainda não roda, pois tem página com erro

--- semana9/Labex/labex/src/Components/HomePage/HomePage.js
+++ semana9/Labex/labex/src/Components/HomePage/HomePage.js
@@ -2,13 +2,27 @@ import React from 'react';
 import HeaderHome from '../Header/HeaderHome';
 import Footer from '../Footer/Footer';
 
-function HomePage() {
+import { useHistory } from "react-router-dom";
+
+const HomePage = () => {
+    const history = useHistory();
+      
+    const TripPage = () => {
+      history.push("/viagens");
+    };
+  
+    const Routers = () => {
+      history.push("/admin");
+    };
+
     return (
         <div>
             <HeaderHome />
             <p>LOGIN USUÁRIO</p>
             <h1>OU</h1>
             <p>NOVO CADASTRO</p>
+            <button onClick={TripPage}>Ir para Viagens</button>
+            <button onClick={Routers}>Ir para Admin</button>
             <Footer />
         </div>
     );
--- semana9/Labex/labex/src/Components/Trips/TripsPage.js
+++ semana9/Labex/labex/src/Components/Trips/TripsPage.js
@@ -2,12 +2,21 @@ import React from 'react';
 import HeaderTrips from '../Header/HeaderTrips';
 import Footer from '../Footer/Footer';
 import BuyTrips from './BuyTrips';
-import TripRegistration from './TripRegistration.js';
+import TripRegistration from './TripRegistration';
 
-function TripPage() {
+import { useHistory } from "react-router";
+
+const TripPage = () => {
+    const history = useHistory();
+  
+    const HomePage = () => {
+      history.replace("/");
+    };
+
     return(
         <div>
             <HeaderTrips/>
+            <button onClick={HomePage}>Ir para HomePage</button>
             <TripRegistration/>
             <BuyTrips/>
             <Footer/>
