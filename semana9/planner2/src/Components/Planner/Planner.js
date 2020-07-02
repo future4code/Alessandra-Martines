7714fa98f963318597260e8b096e1c9e600b051a
alessandra-leka <leka.produtora@gmail.com>
2020-07-02 17:00:39 -0300

    Corrigindo o planner

--- semana9/planner2/src/Components/Planner/Planner.js
+++ semana9/planner2/src/Components/Planner/Planner.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function Planner() {
+const Semanas = styled.div`
+display: flex;
+border: 1px solid black;
+`
+
+function Planner (){
     return(
         <div>
-            <p>Nova tarefa: </p> 
-            <input id="meu-input" type="text" onchange="adicionarItem"> </input>
-                <select name="dia-da-semana">
+            <h1>PLANNER</h1>
+            <div className="tarefas">
+                <label>Nova tarefa: </label>
+                <input type="text" placeholder="Adicionar tarefa"></input>
+                <button >Adicionar</button>
+            </div>
+            
+            <select className="Select">
                     <option value="Segunda">Segunda</option>
                     <option value="Terça">Terça</option>
                     <option value="Quarta">Quarta</option>
@@ -13,22 +24,20 @@ function Planner() {
                     <option value="Sexta">Sexta</option>
                     <option value="Sábado">Sábado</option>
                     <option value="Domingo">Domingo</option>
-                </select>
-        <button onclick="adicionarItem()">Adicionar</button>
-    
-            <div id="dia-da-semana" class="container">
-                <ul class="s1">Segunda</ul>
-                <ul class="s2">Terça</ul>
-                <ul class="s3">Quarta</ul>
-                <ul class="s4">Quinta</ul>
-                <ul class="s5">Sexta</ul>
-                <ul class="s6">Sábado</ul>
-                <ul class="s7">Domingo</ul>
+            </select>
+            
+            <div className="Semanas">
+                <h3>SEGUNDA-FEIRA</h3>
+                <h3>TERÇA-FEIRA</h3>
+                <h3>QUARTA-FEIRA</h3>
+                <h3>QUINTA-FEIRA</h3>
+                <h3>SEXTA-FEIRA</h3>
+                <h3>SÁBADO</h3>
+                <h3>DOMINGO</h3>
             </div>
-        
-            <footer> Sempre planeje seu dia escrevendo!</footer>
+            
         </div>
-    );
+    )
 }
 
 export default Planner;
