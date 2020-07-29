e64041e711002ac7a02719e39d5382894bc0b118
alessandra-leka <leka.produtora@gmail.com>
2020-07-29 16:51:15 -0300

    Resolução do exercício 1 rodando

---
+++ semana14/aula46/src/exercicio2.ts
@@ -0,0 +1,4 @@
+/* Crie uma função que recebe o array de eventos criado no exercício anterior, e imprime algumas informações sobre 
+cada um no console
+
+a) Exiba as informações do evento como uma string com o seguinte formato: */
