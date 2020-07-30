fb9c10c422fb62fffd3e01d4485bd79f393c6500
alessandra-leka <leka.produtora@gmail.com>
2020-07-29 23:52:01 -0300

    Resolução do 2, 3 e 4

--- semana14/aula46/src/exercicio2.ts
+++
@@ -1,4 +0,0 @@
-/* Crie uma função que recebe o array de eventos criado no exercício anterior, e imprime algumas informações sobre 
-cada um no console
-
-a) Exiba as informações do evento como uma string com o seguinte formato: */
